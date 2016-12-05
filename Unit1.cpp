7706f7461c0bb51f20c8b8890eeffbdf3d908a65
Ivan123123 <ivanivanbo@mail.ru>
2016-12-05 16:01:02 +0300

    v2

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,1 +1,23 @@
+#include <iostream.h>
 
+#include <fstream.h>
+
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+int N, i=0, sum=0, dfb; //dfb - dlina faila v baitah
+
+ifstream fin("c:\\os\\bin.dat", ios::binary);
+
+for(i=0; i<N; i++) cout << arr[i] << " ";
+
+for(i=0; i<N; i++) sum=sum+arr[i];
+
+cout << "\n Ih summa = " << sum;
+
+getch(); return 0;
+
+}
