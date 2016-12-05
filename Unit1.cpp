c9df13caa8300058669e4e73615c5fba8f71093b
Ivan123123 <ivanivanbo@mail.ru>
2016-12-05 16:06:58 +0300

    v3

--- Unit1.cpp
+++ Unit1.cpp
@@ -12,6 +12,21 @@ int N, i=0, sum=0, dfb; //dfb - dlina faila v baitah
 
 ifstream fin("c:\\os\\bin.dat", ios::binary);
 
+if(!fin) { cout << "Oshibka otkrytiya faila!"; getch(); return 1; }
+
+fin.seekg(0, ios::end); //usanavlivaem poziciichetenia yf conec faila(ot conca 0 bait)
+
+dfb = fin.tellg(); //poluchaem znachenie pozicii conca faila (v bait)
+
+N=dfb/4; //znaya. chto celoe chislo zanimaem 4 baita vichislyaem kol-vo chisel
+
+int *arr = new int [N]; //sozdaem dinamik massiv
+
+fin.seekg(0, ios::beg); //perad chteniem dannih peremechaem tekuwyy poziciy na nachalo faila
+fin.read(reinterpret_cast<char*>(arr), dfb);
+
+cout << "Iz faila schitano " << N << " elementov:" << endl;
+
 for(i=0; i<N; i++) cout << arr[i] << " ";
 
 for(i=0; i<N; i++) sum=sum+arr[i];
